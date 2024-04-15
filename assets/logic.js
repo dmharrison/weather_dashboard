098b25280b9cab5959fcaafece23272c19e648fc
dmharrison <dmharrison2014@gmail.com>
2024-04-15 16:30:29 -0400

    wrote asych request code to fetch geo data from endpoint

--- assets/logic.js
+++ assets/logic.js
@@ -1,1 +1,25 @@
 console.log("you are connected");
+//function to fetch weather data
+function fetchWeather(city, state, country) {
+  const apiKey = "a34bc89233dbec616ec7e18bb9ab5a30";
+  const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},${country}&limit=1&appid=${apiKey}`;
+  return fetch(geoUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      return data;
+    })
+    .catch((error) => {
+      console.error("Error fetching geographical data:", error);
+      throw error;
+    });
+}
+
+// Call fetchWeather and log the data
+fetchWeather("Marietta", "GA", "US")
+  .then((data) => {
+    console.log(data);
+    // Continue with your code logic here
+  })
+  .catch((error) => {
+    console.error("Error occurred:", error);
+  });
